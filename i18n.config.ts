64edbd457f223625e2580ec942b671671f193f2a
Robert Kozik <robert.kozik@swmansion.com>
2021-10-04 17:32:18 +0200

    create translation config file

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { en } from "./translations";
-//empty for now
+
 const resources = { 
   en: {
     translations: en,
@@ -10,10 +10,9 @@ const resources = {
 
 i18n.use(initReactI18next).init({
   resources,
-  //language to use if translations in user language are not available
   fallbackLng: "en",
   interpolation: {
-    escapeValue: false, // not needed for react!!
+    escapeValue: false,
   },
 });
 
