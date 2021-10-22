52c7ad5a239764db871d0d6068992977c1baeee6
Robert Kozik <robert.kozik@swmansion.com>
2021-10-22 19:14:54 +0200

    add react-native-elements with support dependencies

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { Home, Notifications } from './pages';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { Home, Notifications } from "./pages";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Notifications" component={Notifications} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Notifications" component={Notifications} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
